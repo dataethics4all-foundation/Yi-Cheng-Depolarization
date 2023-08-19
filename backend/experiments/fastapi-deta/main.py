545df76764ef86a38f43c9816797579a1aa8e3a3
Yi Cheng <0418yicheng@gmail.com>
2023-08-19 13:32:07 -0400

    Added langchain

--- backend/experiments/fastapi-deta/main.py
+++ backend/experiments/fastapi-deta/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage
 
 app = FastAPI()
+chat = ChatOpenAI(openai_api_keys="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")
 
 
 @app.get("/")
@@ -12,3 +15,7 @@ def read_root():
 def read_item(item_id: int):
     return {"item_id": item_id}
 
+@app.get("/query")
+def get_response(message):
+    result = chat.generate(SystemMessage(content="message"))
+    return result
